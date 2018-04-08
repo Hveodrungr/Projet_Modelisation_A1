251b215c61723bb68a7489db2e106505587e7c5b
ThomasSou <33135303+ThomasSou@users.noreply.github.com>
2018-04-08 23:03:21 +0200

    Modif nom de dossier

--- Maintenir-gerer_BDD/SQL_Query_4.sql
+++
@@ -1,6 +0,0 @@
-SELECT vente.ID_Province, COUNT(vente.ID_Vente) as `Vente total`, Moyenne.`Moyenne vente`, (COUNT(vente.ID_Vente) / Moyenne.`Moyenne vente`) as `Vente Total/Moyenne des ventes `
-
-FROM vente,
-(SELECT (COUNT(venteProvince.ID_Vente) / count(DISTINCT(venteProvince.ID_Province))) as `Moyenne vente`
-FROM vente as venteProvince) as Moyenne
-GROUP BY vente.ID_Province;
